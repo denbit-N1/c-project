44c185ce17d8a3618a01b09334fb0a302b86b0f5
SUVAM SAMANTA <suvamsamanta06@gmail.com>
2025-06-22 17:56:56 +0530

    Edit

--- management.c
+++ management.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int new_user = -1;
+int user_no = -1;
 struct user
 {
     char username[20];
@@ -10,20 +10,59 @@ struct user
 
 void user_register()
 {
-    new_user++;
-    FILE *fp;
-    fp=fopen("register_data.txt","a");
-    if (fp==NULL)
-        exit(1);
+    user_no++;
+    FILE *un, *ps;
+    char ch,ch2,arr[10][20],arr2[10][18];
+    int col=0;
+    un=fopen("username.txt","a+");
+    ps=fopen("password.txt","a+");
+    while (1)
+    {
+        ch = fgetc(un);
+        if (ch==EOF)
+            break;
+        if (ch=='\n')
+        {
+            arr[user_no][col]='\0';
+            strcpy(user[user_no].username, arr[user_no]);
+            user_no++;
+            col = 0;
+        }
+        else
+        {
+            arr[user_no][col]=ch;
+            col++;
+        } 
+    }
+    user_no=0;
+    while (1)
+    {
+        ch2 = fgetc(ps);
+        if (ch2==EOF)
+            break;
+        if (ch2=='\n')
+        {
+            arr2[user_no][col]='\0';
+            strcpy(user[user_no].password, arr2[user_no]);
+            user_no++;
+            col = 0;
+        }
+        else
+        {
+            arr2[user_no][col]=ch2;
+            col++;
+        } 
+    }
     printf("Enter your username : ");
-    scanf("%s",user[new_user].username);
-    fprintf(fp,"%s",user[new_user].username);
-    fputs(" ",fp);
+    scanf("%s",user[user_no].username);
+    fprintf(un,"%s",user[user_no].username);
+    fputs("\n",un);
     printf("Enter your password : ");
-    scanf("%s",user[new_user].password);
-    fprintf(fp,"%s",user[new_user].password);
-    fputs("\n",fp);
-    fclose(fp);
+    scanf("%s",user[user_no].password);
+    fprintf(ps,"%s",user[user_no].password);
+    fputs("\n",ps);
+    fclose(un);
+    fclose(ps);
     printf("Registration successful!\n");
 }
 void user_login()
@@ -33,7 +72,7 @@ void user_login()
     scanf("%s",username);
     printf("Enter password : ");
     scanf("%s",password);
-    if (strcmp(user[new_user].username,username)==0 && strcmp(user[new_user].password,password)==0)
+    if (strcmp(user[user_no].username,username)==0 && strcmp(user[user_no].password,password)==0)
     {
         printf("Login successful! Welcome, %s!\n",username);
     }
